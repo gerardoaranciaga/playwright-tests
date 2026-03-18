947d37d68a6bd9abfd320eb4fd4cb25ad0066aeb
Gerardo <gerardoaranciaga60@gmail.com>
2026-03-18 12:02:49 -0300

    s

--- pageObjets/ImDBHome.js
+++ pageObjets/ImDBHome.js
@@ -10,6 +10,8 @@ class ImDBHome{
     
     async search(movieName){
         const searchBar = await this.page.locator('#suggestion-search');
+        await expect(searchBar).toBeVisible();   // espera real
+        await expect(searchBar).toBeEnabled();
         await searchBar.fill(movieName);
         await searchBar.press('Enter');
     }
